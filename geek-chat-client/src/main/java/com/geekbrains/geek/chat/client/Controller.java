a276069d6aaa506d39931d3b3a138310641d25e2
denis.volkov <denis.volkov@accenture.com>
2021-08-03 16:19:46 +0300

    47:44

--- geek-chat-client/src/main/java/com/geekbrains/geek/chat/client/Controller.java
+++ geek-chat-client/src/main/java/com/geekbrains/geek/chat/client/Controller.java
@@ -1,4 +1,30 @@
 package com.geekbrains.geek.chat.client;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @FXML
+    public TextField msgField;
+    @FXML
+    public TextArea mainArea;
+
+    private Network network;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        network = new Network(); //В идеале инициализацию с помощью кнопки
+    }
+
+    public void sendMsgAction(ActionEvent actionEvent) {
+        network.sendMessage(msgField.getText());
+        msgField.clear();
+        msgField.requestFocus();
+    }
 }
--- geek-chat-client/src/main/java/com/geekbrains/geek/chat/client/Network.java
+++ geek-chat-client/src/main/java/com/geekbrains/geek/chat/client/Network.java
@@ -7,9 +7,14 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
 
 public class Network {
     private SocketChannel channel;
+
+    private static final String HOST = "localhost";
+    private static final int PORT = 8189;
 
     public Network() {
         new Thread(() -> {
@@ -22,9 +27,10 @@ public class Network {
                             @Override
                             protected void initChannel(SocketChannel socketChannel) throws Exception {
                                 channel = socketChannel;
+                                socketChannel.pipeline().addLast(new StringDecoder(), new StringEncoder());
                             }
                         });
-                ChannelFuture future = b.connect("localhost", 8189).sync();
+                ChannelFuture future = b.connect(HOST, PORT).sync();
                 future.channel().closeFuture().sync();
             } catch (InterruptedException e) {
                 e.printStackTrace();
